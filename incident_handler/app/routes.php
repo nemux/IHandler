a141e17a75f959edb300e79729b9bd5b10532fed
Diego Miguel Angel López Muñoz <dlopez@globalcybersec.com>
2015-04-09 11:58:04 -0500

    - Generación de estadísticas por sensor -

--- incident_handler/app/routes.php
+++ incident_handler/app/routes.php
@@ -10,119 +10,123 @@
 |
 */
 
-Route::get('/','HomeController@index');
+Route::get('/', 'HomeController@index');
 
-Route::get('dashboard',array('before'=> 'auth', 'uses' => 'HomeController@dashboard'));
+Route::get('dashboard', array('before' => 'auth', 'uses' => 'HomeController@dashboard'));
 
-Route::get('rule/query/{id}', 'RuleController@query')->where(array('id'=>'^[0-9]+$'));
-Route::get('occurence/query/{id}', 'OccurenceController@query')->where(array('id'=>'^[0-9]+$'));
+Route::get('rule/query/{id}', 'RuleController@query')->where(array('id' => '^[0-9]+$'));
+Route::get('occurence/query/{id}', 'OccurenceController@query')->where(array('id' => '^[0-9]+$'));
 
-Route::get('login','LoginController@login');
+Route::get('login', 'LoginController@login');
 Route::post('login', 'LoginController@doLogin');
 Route::get('logout', 'LoginController@logout');
 
 
-Route::group(array('before'=>'admin', 'prefix'=>'handler'),function(){
+Route::group(array('before' => 'admin', 'prefix' => 'handler'), function () {
 
-  # User Routes
-  Route::get('/', 'IncidentHandlerController@index');
-  Route::get('create', 'IncidentHandlerController@create');
-  Route::post('create', 'IncidentHandlerController@create');
-  Route::get('update/{id}', 'IncidentHandlerController@getUpdate')->where(array('id'=>'^[0-9]+$'));
-  Route::post('update', 'IncidentHandlerController@postUpdate');
-  Route::get('view/{id}', 'IncidentHandlerController@view')->where(array('id'=>'^[0-9]+$'));
+    # User Routes
+    Route::get('/', 'IncidentHandlerController@index');
+    Route::get('create', 'IncidentHandlerController@create');
+    Route::post('create', 'IncidentHandlerController@create');
+    Route::get('update/{id}', 'IncidentHandlerController@getUpdate')->where(array('id' => '^[0-9]+$'));
+    Route::post('update', 'IncidentHandlerController@postUpdate');
+    Route::get('view/{id}', 'IncidentHandlerController@view')->where(array('id' => '^[0-9]+$'));
 
- // Route::get('larala','IncidentHandlerController@aproved');
-  #Admin Routes
+    // Route::get('larala','IncidentHandlerController@aproved');
+    #Admin Routes
 });
 Route::post('handler/update/password', 'IncidentHandlerController@passwordUpdate');
 Route::post('handler/send/token', 'IncidentHandlerController@sendToken');
 
-Route::group(array('before'=>'admin', 'prefix'=>'sensor'),function(){
+Route::group(array('before' => 'admin', 'prefix' => 'sensor'), function () {
 
-  # User Routes
-  Route::get('/', 'SensorController@index');
-  Route::get('create', 'SensorController@create');
-  Route::post('create', 'SensorController@create');
-  Route::get('update/{id}', 'SensorController@getUpdate')->where(array('id'=>'^[0-9]+$'));
-  Route::post('update', 'SensorController@postUpdate');
-  Route::get('view/{id}', 'SensorController@view')->where(array('id'=>'^[0-9]+$'));
+    # User Routes
+    Route::get('/', 'SensorController@index');
+    Route::get('create', 'SensorController@create');
+    Route::post('create', 'SensorController@create');
+    Route::get('update/{id}', 'SensorController@getUpdate')->where(array('id' => '^[0-9]+$'));
+    Route::post('update', 'SensorController@postUpdate');
+    Route::get('view/{id}', 'SensorController@view')->where(array('id' => '^[0-9]+$'));
 
-  #Admin Routes
+    #Admin Routes
 });
 
-Route::group(array('before'=>'auth', 'prefix'=>'incident'),function(){
-  Route::get('/', 'IncidentController@index');
-  Route::get('create', 'IncidentController@create');
-  Route::post('create', 'IncidentController@create');
-  Route::get('update/{id}', 'IncidentController@getUpdate')->where(array('id'=>'^[0-9]+$'));
-  Route::post('update', 'IncidentController@postUpdate');
-  Route::post('updateStatus', 'IncidentController@updateStatus');
-  Route::get('manage/{id}', 'IncidentController@edit');
-  Route::post('manage', 'IncidentController@change_status');
-  Route::get('pdf/{id}', 'IncidentController@pdf');
-  Route::get('doc/{id}', 'IncidentController@doc');
-  Route::get('doc', 'IncidentController@docReport');
-  Route::post('add/recomendation', 'IncidentController@addRecomendation');
-  Route::post('add/Observation', 'IncidentController@addObservation');
-  Route::get('open/', 'IncidentController@openStatus');
-  Route::get('investigation/', 'IncidentController@investigationStatus');
-  Route::get('solved/', 'IncidentController@solvedStatus');
-  //Route::get('show/{id}', 'IncidentController@pdf');
-  Route::get('sensor/get/{id}','SensorController@customerSensor');
-  Route::post('add/Annex', 'IncidentController@addAnnex');
-  Route::get('del/Annex/{id}', 'IncidentController@delAnnex');
-  Route::get('search/ip/', 'IncidentController@searchIp');
-  Route::post('search/render/ip/', 'IncidentController@renderIp');
-  Route::get('mail/{id}','IncidentController@mail');
+Route::group(array('before' => 'auth', 'prefix' => 'incident'), function () {
+    Route::get('/', 'IncidentController@index');
+    Route::get('create', 'IncidentController@create');
+    Route::post('create', 'IncidentController@create');
+    Route::get('update/{id}', 'IncidentController@getUpdate')->where(array('id' => '^[0-9]+$'));
+    Route::post('update', 'IncidentController@postUpdate');
+    Route::post('updateStatus', 'IncidentController@updateStatus');
+    Route::get('manage/{id}', 'IncidentController@edit');
+    Route::post('manage', 'IncidentController@change_status');
+    Route::get('pdf/{id}', 'IncidentController@pdf');
+    Route::get('doc/{id}', 'IncidentController@doc');
+    Route::get('doc', 'IncidentController@docReport');
+    Route::post('add/recomendation', 'IncidentController@addRecomendation');
+    Route::post('add/Observation', 'IncidentController@addObservation');
+    Route::get('open/', 'IncidentController@openStatus');
+    Route::get('investigation/', 'IncidentController@investigationStatus');
+    Route::get('solved/', 'IncidentController@solvedStatus');
+    //Route::get('show/{id}', 'IncidentController@pdf');
+    Route::get('sensor/get/{id}', 'SensorController@customerSensor');
+    Route::post('add/Annex', 'IncidentController@addAnnex');
+    Route::get('del/Annex/{id}', 'IncidentController@delAnnex');
+    Route::get('search/ip/', 'IncidentController@searchIp');
+    Route::post('search/render/ip/', 'IncidentController@renderIp');
+    Route::get('mail/{id}', 'IncidentController@mail');
 
-  Route::get('view/filter/', 'IncidentController@monthly');
-  Route::post('view/filter/','IncidentController@viewMonthly');
+    Route::get('view/filter/', 'IncidentController@monthly');
+    Route::post('view/filter/', 'IncidentController@viewMonthly');
 
-  Route::get('view/sensor/', 'IncidentController@sensor');
-  Route::post('view/sensor/','IncidentController@viewSensor');
+    Route::get('view/sensor/', 'IncidentController@sensor');
+    Route::post('view/sensor/', 'IncidentController@viewSensor');
 
 });
 
-Route::group(array('before'=>'admin', 'prefix'=>'customer'),function(){
+Route::group(array('before' => 'admin', 'prefix' => 'customer'), function () {
 
-  # User Routes
-  Route::get('/', 'CustomerController@index');
-  Route::get('create', 'CustomerController@create');
-  Route::post('create', 'CustomerController@create');
-  Route::get('update/{id}', 'CustomerController@getUpdate')->where(array('id'=>'^[0-9]+$'));
-  Route::post('update', 'CustomerController@postUpdate');
-  Route::get('view/{id}', 'CustomerController@view')->where(array('id'=>'^[0-9]+$'));
-  Route::post('importCustomers', 'OtrsController@importCustomers');
+    # User Routes
+    Route::get('/', 'CustomerController@index');
+    Route::get('create', 'CustomerController@create');
+    Route::post('create', 'CustomerController@create');
+    Route::get('update/{id}', 'CustomerController@getUpdate')->where(array('id' => '^[0-9]+$'));
+    Route::post('update', 'CustomerController@postUpdate');
+    Route::get('view/{id}', 'CustomerController@view')->where(array('id' => '^[0-9]+$'));
+    Route::post('importCustomers', 'OtrsController@importCustomers');
 
-  #Admin Routes
+    #Admin Routes
 });
-Route::group(array('before'=>'auth', 'prefix'=>'stats'),function(){
-  # User Routes
-  Route::get('/incident', 'StatsController@incident');
-  Route::post('/incident/graph', 'StatsController@incidentGraph');
-  Route::get('/ip', 'StatsController@ip');
-  Route::post('/ip/graph', 'StatsController@ipGraph');
-  Route::get('/attack', 'StatsController@attack');
-  Route::post('/attack/graph', 'StatsController@attackGraph');
-  Route::get('/category', 'StatsController@category');
-  Route::post('/category/graph', 'StatsController@categoryGraph');
-  Route::get('/handler', 'StatsController@handler');
-  Route::post('/handler/graph', 'StatsController@handlerGraph');
-  Route::get('/severity', 'StatsController@severity');
-  Route::post('/severity/graph', 'StatsController@severityGraph');
-  Route::get('/blacklist', 'StatsController@blacklist');
-  Route::get('/blacklist/doc', 'StatsController@doc');
-  Route::post('/ip/origin', 'StatsController@post_IPListByOrigin');
-  Route::get('/ip/origin', 'StatsController@get_IPListByOrigin');
+Route::group(array('before' => 'auth', 'prefix' => 'stats'), function () {
+    # User Routes
+    Route::get('/incident', 'StatsController@incident');
+    Route::post('/incident/graph', 'StatsController@incidentGraph');
+    Route::get('/ip', 'StatsController@ip');
+    Route::post('/ip/graph', 'StatsController@ipGraph');
+    Route::get('/attack', 'StatsController@attack');
+    Route::post('/attack/graph', 'StatsController@attackGraph');
+    Route::get('/category', 'StatsController@category');
+    Route::post('/category/graph', 'StatsController@categoryGraph');
+    Route::get('/handler', 'StatsController@handler');
+    Route::post('/handler/graph', 'StatsController@handlerGraph');
+    Route::get('/severity', 'StatsController@severity');
+    Route::post('/severity/graph', 'StatsController@severityGraph');
+
+    Route::get('/sensor', 'StatsController@sensor'); //Obtiene la vista de estadísticas por sensor
+    Route::post('/sensor/graph','StatsController@sensorGraph'); //Envía la información para generar las gráficas por sensor
+
+    Route::get('/blacklist', 'StatsController@blacklist');
+    Route::get('/blacklist/doc', 'StatsController@doc');
+    Route::post('/ip/origin', 'StatsController@post_IPListByOrigin');
+    Route::get('/ip/origin', 'StatsController@get_IPListByOrigin');
 
 
-  #Admin Routes
+    #Admin Routes
 });
 
-Route::get('incident/view/{id}','IncidentController@view');
+Route::get('incident/view/{id}', 'IncidentController@view');
 
-Route::group(array('before'=>'admin', 'prefix'=>'attack'),function() {
+Route::group(array('before' => 'admin', 'prefix' => 'attack'), function () {
     Route::get('create', 'AttackController@get_create');
     Route::post('create', 'AttackController@post_create');
     Route::get('/', 'AttackController@index');
@@ -133,26 +137,25 @@ Route::group(array('before'=>'admin', 'prefix'=>'attack'),function() {
 });
 
 
-
-Route::group(array('before'=>'auth', 'prefix'=>'report'),function(){
-  # Report Routes
-  Route::get('/', 'ReportController@index');
-  Route::get('/{type}', 'ReportController@view')->where(array('type' => '(date|handler|category|severity|status|ip|csv)'));
-  Route::post('/create/{doc_type}', 'ReportController@create')->where(array('doc_type' => '(doc|csv)'));
-  Route::get('/ip/doc', 'ReportController@ipDoc');
+Route::group(array('before' => 'auth', 'prefix' => 'report'), function () {
+    # Report Routes
+    Route::get('/', 'ReportController@index');
+    Route::get('/{type}', 'ReportController@view')->where(array('type' => '(date|handler|category|severity|status|ip|csv)'));
+    Route::post('/create/{doc_type}', 'ReportController@create')->where(array('doc_type' => '(doc|csv)'));
+    Route::get('/ip/doc', 'ReportController@ipDoc');
 });
 
 
-Route::group(array('before'=>'admin', 'prefix' => 'otrs'), function(){
-  Route::get('import','OtrsController@import');
-  Route::get('result','OtrsController@result');
+Route::group(array('before' => 'admin', 'prefix' => 'otrs'), function () {
+    Route::get('import', 'OtrsController@import');
+    Route::get('result', 'OtrsController@result');
 });
 
-Route::group(array('prefix' => 'api'), function(){
-    Route::group(array('prefix' => '1.1'), function() {
+Route::group(array('prefix' => 'api'), function () {
+    Route::group(array('prefix' => '1.1'), function () {
         Route::group(array('prefix' => 'task'), function () {
             Route::post('/ticket/reminder', 'TaskController@sendReminder');
-            Route::post('/ticket/close','TaskController@closeTickets');
+            Route::post('/ticket/close', 'TaskController@closeTickets');
             Route::get('/otrs/ticket/create/{key}/{id}', 'OtrsController@sendTicket');
         });
     });
--- incident_handler/app/views/include/sidebar.blade.php
+++ incident_handler/app/views/include/sidebar.blade.php
@@ -1,144 +1,145 @@
 <div id="sidebar" class="sidebar">
-<!-- begin sidebar scrollbar -->
-			<div data-scrollbar="true" data-height="100%">
-				<!-- begin sidebar user -->
-				<ul class="nav">
-					<li class="nav-profile">
-						<div class="image">
-							<a href="javascript:;"><img src="/assets/img/handler.png" alt="" /></a>
-						</div>
-						<div class="info">
-							Incident Handler
-						</div>
-            {{ Auth::user()->username." "}}
-					</li>
-				</ul>
-				<!-- end sidebar user -->
-				<!-- begin sidebar nav -->
-				<ul class="nav">
-					<li class="nav-header">Menú</li>
-					<li class="has-sub ">
-						<a href="javascript:;">
-								<b class="caret pull-right"></b>
-								<i class="fa fa-bookmark"></i>
-								<span>Incidentes</span>
+    <!-- begin sidebar scrollbar -->
+    <div data-scrollbar="true" data-height="100%">
+        <!-- begin sidebar user -->
+        <ul class="nav">
+            <li class="nav-profile">
+                <div class="image">
+                    <a href="javascript:;"><img src="/assets/img/handler.png" alt=""/></a>
+                </div>
+                <div class="info">
+                    Incident Handler
+                </div>
+                {{ Auth::user()->username." "}}
+            </li>
+        </ul>
+        <!-- end sidebar user -->
+        <!-- begin sidebar nav -->
+        <ul class="nav">
+            <li class="nav-header">Menú</li>
+            <li class="has-sub ">
+                <a href="javascript:;">
+                    <b class="caret pull-right"></b>
+                    <i class="fa fa-bookmark"></i>
+                    <span>Incidentes</span>
 
-						</a>
-						<ul class="sub-menu">
-								<li class=""><a href="/incident/create">Crear</a></li>
-								<li><a href="/incident/view/filter">Ver ...</a></li>
+                </a>
+                <ul class="sub-menu">
+                    <li class=""><a href="/incident/create">Crear</a></li>
+                    <li><a href="/incident/view/filter">Ver ...</a></li>
 
-								<li><a href="/incident/">Ver Todos</a></li>
-								<li><a href="/incident/search/ip">Buscar por Ip</a></li>
-						</ul>
-					</li>
-					<li class="has-sub ">
-							<a href="javascript:;">
-									<b class="caret pull-right"></b>
-									<i class="fa fa-file-text "></i>
-									<span>Reporte de Incidentes</span>
-							</a>
-							<ul class="sub-menu">
-									<li><a href="/report/date">Por Fecha</a></li>
-									<li><a href="/report/handler">Por Handler</a></li>
-									<li><a href="/report/category">Por Categoría</a></li>
-									<li><a href="/report/severity">Por Severidad</a></li>
-									<li><a href="/report/status">Por Status</a></li>
-                                    <li><a href="/report/ip">Por IP</a></li>
-                                    <li><a href="/report/csv">CSV de Tickets</a></li>
-									<li><a href="/incident/view/sensor">Lista Por Sensor</a></li>
-							</ul>
-					</li>
-          @if (Auth::user()->type->name == 'admin')
-					<li class="has-sub ">
-						<a href="javascript:;">
-						    <b class="caret pull-right"></b>
-						    <i class="fa fa-user"></i>
-						    <span>Incident Handler</span>
-					    </a>
-						<ul class="sub-menu">
-						    <li><a href="/handler/create">Crear</a></li>
-						    <li><a href="/handler/">Ver</a></li>
-						</ul>
-					</li>
+                    <li><a href="/incident/">Ver Todos</a></li>
+                    <li><a href="/incident/search/ip">Buscar por Ip</a></li>
+                </ul>
+            </li>
+            <li class="has-sub ">
+                <a href="javascript:;">
+                    <b class="caret pull-right"></b>
+                    <i class="fa fa-file-text "></i>
+                    <span>Reporte de Incidentes</span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="/report/date">Por Fecha</a></li>
+                    <li><a href="/report/handler">Por Handler</a></li>
+                    <li><a href="/report/category">Por Categoría</a></li>
+                    <li><a href="/report/severity">Por Severidad</a></li>
+                    <li><a href="/report/status">Por Status</a></li>
+                    <li><a href="/report/ip">Por IP</a></li>
+                    <li><a href="/report/csv">CSV de Tickets</a></li>
+                    <li><a href="/incident/view/sensor">Lista Por Sensor</a></li>
+                </ul>
+            </li>
+            @if (Auth::user()->type->name == 'admin')
+                <li class="has-sub ">
+                    <a href="javascript:;">
+                        <b class="caret pull-right"></b>
+                        <i class="fa fa-user"></i>
+                        <span>Incident Handler</span>
+                    </a>
+                    <ul class="sub-menu">
+                        <li><a href="/handler/create">Crear</a></li>
+                        <li><a href="/handler/">Ver</a></li>
+                    </ul>
+                </li>
 
-          <li class="has-sub ">
-						<a href="javascript:;">
-						    <b class="caret pull-right"></b>
-						    <i class="fa fa-circle-o-notch"></i>
-						    <span>OTRS</span>
-					    </a>
-						<ul class="sub-menu">
-						    <li><a href="/otrs/import">Importar Informaci&oacute;n</a></li>
-						</ul>
-					</li>
+                <li class="has-sub ">
+                    <b class="caret pull-right"></b>
+                    <i class="fa fa-circle-o-notch"></i>
+                    <span>OTRS</span>
+                    </a>
+                    <ul class="sub-menu">
+                        <li><a href="/otrs/import">Importar Informaci&oacute;n</a></li>
+                    </ul>
+                </li>
 
 
-					<li class="has-sub ">
-						<a href="javascript:;">
-								<b class="caret pull-right"></b>
-								<i class="fa fa-eye"></i>
-								<span>Sensores</span>
-							</a>
-						<ul class="sub-menu">
-								<li><a href="/sensor/create">Crear</a></li>
-								<li><a href="/sensor/">Ver</a></li>
-						</ul>
-					</li>
+                <li class="has-sub ">
+                    <a href="javascript:;">
+                        <b class="caret pull-right"></b>
+                        <i class="fa fa-eye"></i>
+                        <span>Sensores</span>
+                    </a>
+                    <ul class="sub-menu">
+                        <li><a href="/sensor/create">Crear</a></li>
+                        <li><a href="/sensor/">Ver</a></li>
+                    </ul>
+                </li>
 
-					<li class="has-sub ">
-						<a href="javascript:;">
-								<b class="caret pull-right"></b>
-								<i class="fa fa-suitcase"></i>
-								<span>Clientes</span>
-							</a>
+                <li class="has-sub ">
+                    <a href="javascript:;">
+                        <b class="caret pull-right"></b>
+                        <i class="fa fa-suitcase"></i>
+                        <span>Clientes</span>
+                    </a>
 
-						<ul class="sub-menu">
-								<li><a href="/customer/">Ver</a></li>
-						</ul>
-					</li>
+                    <ul class="sub-menu">
+                        <li><a href="/customer/">Ver</a></li>
+                    </ul>
+                </li>
 
-                    <li class="has-sub ">
-						<a href="javascript:;">
-						    <b class="caret pull-right"></b>
-						    <i class="fa fa-crosshairs "></i>
-						    <span>Attacks</span>
-					    </a>
-						<ul class="sub-menu">
-                            <li><a href="/attack/create">Agregar ataque</a></li>
-						    <li><a href="/attack">Ver ataques</a></li>
-						</ul>
-					</li>
-					@endif
-                    @if (Auth::user()->type->name == 'admin' || Auth::user()->type->name == 'user_2')
-					<li class="has-sub ">
-							<a href="javascript:;">
-								<b class="caret pull-right"></b>
-								<i class="fa fa-bar-chart-o"></i>
-								<span>Estadísticas</span>
-							</a>
+                <li class="has-sub ">
+                    <a href="javascript:;">
+                        <b class="caret pull-right"></b>
+                        <i class="fa fa-crosshairs "></i>
+                        <span>Attacks</span>
+                    </a>
+                    <ul class="sub-menu">
+                        <li><a href="/attack/create">Agregar ataque</a></li>
+                        <li><a href="/attack">Ver ataques</a></li>
+                    </ul>
+                </li>
+            @endif
+            @if (Auth::user()->type->name == 'admin' || Auth::user()->type->name == 'user_2')
+                <li class="has-sub ">
+                    <a href="javascript:;">
+                        <b class="caret pull-right"></b>
+                        <i class="fa fa-bar-chart-o"></i>
+                        <span>Estadísticas</span>
+                    </a>
 
-						<ul class="sub-menu">
-								<li><a href="/stats/incident">De incidentes</a></li>
-								<li><a href="/stats/ip">De direcciones IP</a></li>
-								<li><a href="/stats/handler">Incidentes por Handler</a></li>
-								<li><a href="/stats/category">Por Categorías</a></li>
-								<li><a href="/stats/severity">Por Severidad</a></li>
-								<li><a href="/stats/attack">Por Ataques</a></li>
+                    <ul class="sub-menu">
+                        <li><a href="/stats/incident">De incidentes</a></li>
+                        <li><a href="/stats/ip">De direcciones IP</a></li>
+                        <li><a href="/stats/handler">Incidentes por Handler</a></li>
+                        <li><a href="/stats/category">Por Categorías</a></li>
+                        <li><a href="/stats/severity">Por Severidad</a></li>
+                        <li><a href="/stats/attack">Por Ataques</a></li>
 
-						</ul>
-					</li>
-                    @endif
+                        <li><a href="/stats/sensor">Por Sensor</a></li>
+                    </ul>
+                </li>
+            @endif
 
-					<li><a href="/stats/blacklist"><i class="fa fa-list"></i> <span>Blacklist</span></a></li>
-                    <li><a href="/stats/ip/origin"><i class="fa fa-cubes"></i> <span>IP Origen/Destino</span></a></li>
+            <li><a href="/stats/blacklist"><i class="fa fa-list"></i> <span>Blacklist</span></a></li>
+            <li><a href="/stats/ip/origin"><i class="fa fa-cubes"></i> <span>IP Origen/Destino</span></a></li>
 
-			        <!-- begin sidebar minify button -->
-					<li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
-			        <!-- end sidebar minify button -->
-				</ul>
-				<!-- end sidebar nav -->
-			</div>
-			<!-- end sidebar scrollbar -->
-		</div>
+            <!-- begin sidebar minify button -->
+            <li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i
+                            class="fa fa-angle-double-left"></i></a></li>
+            <!-- end sidebar minify button -->
+        </ul>
+        <!-- end sidebar nav -->
+    </div>
+    <!-- end sidebar scrollbar -->
+</div>
 <div class="sidebar-bg"></div>
