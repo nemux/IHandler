fc77e41c5104f07dbbb1f4c6c838056caa6a157a
Diego Miguel Angel López Muñoz <dlopez@globalcybersec.com>
2015-08-20 18:19:43 -0500

    -- Se agregó la nueva tabla SIGNATURES donde se almacenan las firmas, descripciones en español, recomendaciones, fuentes y riesgos.

---
+++ incident_handler/app/database/migrations/2015_08_20_181308_create_signatures_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSignaturesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('signatures', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->string('signature');
+			$table->text('description')->nullable();
+			$table->text('recommendation')->nullable();
+			$table->text('reference')->nullable();
+			$table->text('risk')->nullable();
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('signatures');
+	}
+
+}
