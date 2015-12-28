a32c015fc48da6cebff1359f9f5362befe23ed50
Diego López <dlopez@globalcybersec.com>
2015-12-28 16:41:59 -0600

    -- Se cambió el favicon route

--- incident_handlerv2/resources/views/layout/dashboard_topmenu.blade.php
+++ incident_handlerv2/resources/views/layout/dashboard_topmenu.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="utf-8">
-    <link rel="shortcut icon" href="custom/favicon.ico">
+    <link rel="shortcut icon" href="{{ asset('custom/favicon.ico') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
