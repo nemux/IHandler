6053d950b3483f781fd689efeca71d2e10a5f09e
Diego López <dlopez@globalcybersec.com>
2015-12-28 20:36:53 -0600

    -- Se agreg{o un archivo de log para nodejs

--- incident_handlerv2/run.sh
+++ incident_handlerv2/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/diego/proyects/handler/incident_handlerv2/
-php artisan serve --host 0.0.0.0 --port 8000  & nodejs socket.js
+php artisan serve --host 0.0.0.0 --port 8000  & nodejs socket.js > nodejs_socket.log &
