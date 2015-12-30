a8284cd5be221408efcc25c41c1bc212d6c1b424
Diego López <dlopez@globalcybersec.com>
2015-12-29 22:56:01 -0600

    --  se corrigieron los parseos de HTML para la generacion de documentos de WORD, removiendo los div, hr, reemplazando <br/> con </p><p> y revisando que los párrafos estén bien formados
    --Corrección en la función replaceTag, no se estaban cerrando las etiquetas

--- incident_handlerv2/app/Library/StringHelper.php
+++ incident_handlerv2/app/Library/StringHelper.php
@@ -14,19 +14,21 @@ class StringHelper
      */
     public static function parseHtml($html)
     {
-        \Log::info($html);
+//        \Log::info($html);
 
         $html = html_entity_decode($html, ENT_QUOTES, 'UTF-8');
-        \Log::info($html);
+//        \Log::info($html);
 
         self::removeAttributes($html);
         self::removeTag('span', $html);
         self::removeTag('i', $html);
         self::replaceTag('b', 'strong', $html);
-        \Log::info($html);
+        self::removeTag('hr', $html);
+        self::removeTag('div', $html);
+//        \Log::info($html);
 
-//        $html = str_replace("<br>", "<br/>", $html);
-//        $html = str_replace("<br />", "<br/>", $html);
+        $html = str_replace("<br>", "<br/>", $html);
+        $html = str_replace("<br />", "<br/>", $html);
         $html = str_replace("<br/>", "</p><p>", $html);
         $html = str_replace(array("\n", "\r", "\r\n"), ' ', $html);
         $html = preg_replace('/<a\b[^>]*>(.*?)<\/a>/i', '$1', $html);
@@ -36,7 +38,9 @@ class StringHelper
         $html = preg_replace('/\s{2,}/', ' ', $html);
         $html = preg_replace('/\n{2,}/', '\n', $html);
         $html = str_replace(array("> <"), array('><'), $html);
-        \Log::info($html);
+
+        self::fixOrphanTag('p', $html);
+//        \Log::info($html);
 
         return $html;
     }
@@ -75,7 +79,7 @@ class StringHelper
     private static function replaceTag($fromtag, $totag, &$html)
     {
         $html = str_replace("<$fromtag>", "<$totag>", $html);
-        $html = str_replace("</$fromtag>", "<$totag>", $html);
+        $html = str_replace("</$fromtag>", "</$totag>", $html);
     }
 
     /**
@@ -86,5 +90,32 @@ class StringHelper
     private static function removeAttributes(&$html)
     {
         $html = preg_replace("/<([a-z][a-z0-9]*)[^>]*?(\\/?)>/", "<$1$2>", $html);
+    }
+
+    /**
+     * Arregla el tag $tag cuando se detecta que está huérfano, para cumplir con un XML well formed
+     *
+     * @param $tag
+     * @param $html
+     */
+    private static function fixOrphanTag($tag, &$html)
+    {
+        $split = preg_split("/<p>/i", $html);
+        $newhtml = '';
+        foreach ($split as $item) {
+            $replaced = preg_replace("/<\\/p>/i", "", $item);
+
+            \Log::info($replaced);
+
+            //Si no empieza y no termina con alguna de estas opciones
+            if (
+                substr($replaced, 0, 4) !== "<ul>" && substr($replaced, -5, 5) !== "</ul>"
+                && substr($replaced, 0, 4) !== "<ol>" && substr($replaced, -5, 5) !== "</ol>"
+                && substr($replaced, 0, 5) !== "<div>" && substr($replaced, -6, 6) !== "</div>"
+            )
+                $replaced = "<$tag>$replaced</$tag>";
+            $newhtml .= $replaced;
+        }
+        $html = $newhtml;
     }
 }
