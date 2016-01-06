0cdde7277dc8e05a53af506c82d0ff5abcad06ac
Diego López <dlopez@globalcybersec.com>
2016-01-06 15:18:40 -0600

    -- Se agregaron mensajes de consola

--- incident_handlerv2/update.sh
+++ incident_handlerv2/update.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+echo "---------------------------------------------"
+echo "| Iniciando la actualización del aplicativo |"
+echo "---------------------------------------------"
 php artisan down; git pull; composer update; php artisan cache:clear; composer dumpautoload -o; php artisan up
+echo "---------------------------------------------"
+echo "| Finalizó  la actualización del aplicativo |"
+echo "---------------------------------------------"
