f4ad6d70e8c71367b7a6db68b96232b5a9c04917
Diego López <dlopez@globalcybersec.com>
2016-01-06 15:13:26 -0600

    -- Se agrego un artisan down y up al inicio y final, respectifvamente, del bash

--- incident_handlerv2/update.sh
+++ incident_handlerv2/update.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git pull; composer update; php artisan cache:clear; composer dumpautoload -o;
+php artisan down; git pull; composer update; php artisan cache:clear; composer dumpautoload -o; php artisan up
