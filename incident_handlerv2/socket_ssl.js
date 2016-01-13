61412a3e9fdbfa9f72cbb953731a4bd7e6237551
Diego López <dlopez@globalcybersec.com>
2016-01-13 12:28:23 -0600

    Se removió el canal no seguro del socket con nodejs

--- incident_handlerv2/socket_ssl.js
+++ incident_handlerv2/socket_ssl.js
@@ -1,24 +1,27 @@
-var express = require('express');
-var https_app = express();
-//var http_app = express();
+//Cargamos el archivo de entorno del socket (.env file)
+var env = require('node-env-file');
+env('.env');
+
+//Cargamos las opciones para el socket io
 var fs = require('fs');
-var Redis = require('ioredis');
 var options = {
     key: fs.readFileSync('./ssl/gcs-imanager.key'),
     cert: fs.readFileSync('./ssl/gcs-imanager.crt'),
     requestCert: true
 };
-var redis = new Redis({
+
+var https_app = require('express')();
+var https_server = require('https').createServer(options, https_app);
+var io = require('socket.io').listen(https_server);
+var redis = require('ioredis')({
     port: process.env.REDIS_PORT,
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD
 });
-//var http = require('http').createServer(http_app);
-var https = require('https').createServer(options, https_app);
-var io = require('socket.io').listen(https);
 
-var env = require('node-env-file');
-env('.env');
+https_server.listen(process.env.NODEJS_PORT_SSL, function () {
+    console.log('Listening on SSL Port ' + process.env.NODEJS_PORT_SSL);
+});
 
 redis.subscribe('test-channel', function (err, count) {
 });
@@ -28,16 +31,3 @@ redis.on('message', function (channel, message) {
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
-
-//http_app.get('*', function (req, res) {
-//    //Redirige el tráfico no seguro por el canal seguro
-//    res.redirect('https://' + process.env.NODEJS_HOST + ':' + process.env.NODEJS_PORT_SSL + req.url)
-//});
-
-https.listen(process.env.NODEJS_PORT_SSL, function () {
-    console.log('Listening on Port ' + process.env.NODEJS_PORT_SSL);
-});
-
-//http.listen(process.env.NODEJS_PORT, function () {
-//console.log('Listening on Port ' + process.env.NODEJS_PORT);
-//});
